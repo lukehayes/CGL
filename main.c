d262791aadef8f880261ca558b7ca5995a1182cd
lukehayes <lukepurplehayes@gmail.com>
2024-09-24 08:37:47 +0100

    Window can how be closed properly

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include "xx/window.h"
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char *argv[])
+{
     Window* window = WindowCreate(800,600, "Title");
 
-    while(1)
+    int RUNNING = 1;
+    while(RUNNING)
     {
+
+        SDL_Event Event;
+        while (SDL_PollEvent(&Event))
+        {
+          if (Event.type == SDL_KEYDOWN)
+          {
+            switch (Event.key.keysym.sym)
+            {
+              case SDLK_ESCAPE:
+                RUNNING = 0;
+                break;
+              default:
+                break;
+            }
+            }
+        }
+
+
         glClear(GL_COLOR_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
         glClearColor(0,0,0,1);
 
