706fe6febfe2d24669a051c3894b18da4feb0d5a
lukehayes <lukepurplehayes@gmail.com>
2024-11-14 16:17:19 +0000

    Moved shader sources to source scope

--- src/xx/gl/debug_program.c
+++ src/xx/gl/debug_program.c
@@ -1,24 +1,24 @@
 #include "xx/gl/debug_program.h"
 #include "stdio.h"
 
+static const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+static const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+
 GLShader GLInitShader()
 {
     GLShader shader;
-
-    const char *vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
-
-    const char *fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColor;\n"
-	"void main()\n"
-	"{\n"
-	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-	"}\n\0";
-
 
 
    // build and compile our shader program
@@ -68,7 +68,7 @@ GLShader GLInitShader()
 GLProgram GLInitProgram()
 {
     GLProgram program;
-    
+
     glGenVertexArrays(1, &program.vao);
     glBindVertexArray(program.vao);
 
