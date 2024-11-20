fe0f137b88e82b38a93ca0b32b7e8b21253c753d
lukehayes <lukepurplehayes@gmail.com>
2024-11-20 10:05:00 +0000

    Changed 'Color' uniform to 'uColor'

--- assets/shaders/FSH-Texture.glsl
+++ assets/shaders/FSH-Texture.glsl
@@ -5,10 +5,10 @@ out vec4 FragColor;
 in vec3 Color;
 in vec2 TexCoord;
 
-uniform vec3 color;
+uniform vec3 uColor;
 uniform sampler2D ourTexture;
   
 void main()
 {
-    FragColor = texture(ourTexture, TexCoord) * vec4(Color, 1.0f);
+    FragColor = texture(ourTexture, TexCoord) * vec4(uColor, 1.0f);
 } 
