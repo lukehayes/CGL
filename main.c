6b4047531e4d2cb87126df65b3fe83238102007d
lukehayes <lukepurplehayes@gmail.com>
2024-11-21 09:38:01 +0000

    Added rotation and scale to Entity struct

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include "xx/gl/debug_program.h"
 #include "xx/gl/texture.h"
 #include "xx/gl/camera3d.h"
+#include "xx/math/rand.h"
 #include "cglm/cglm.h"
+#include <cglm/affine-pre.h>
 
 typedef struct Entity
 {
     vec3 position;
+    vec3 rotation;
+    vec3 scale;
     vec3 color;
 } Entity;
-
 
 
 void Draw(GLProgram* program, Entity* entity, Camera3D* camera)
@@ -20,6 +23,8 @@ void Draw(GLProgram* program, Entity* entity, Camera3D* camera)
 
     // glm_rotated(modelMatrix, sin(c1) / 10.0, (vec3){1,1,1});
     glm_translate(modelMatrix, entity->position);
+
+    glm_scale(modelMatrix, entity->scale);
 
     glBindTexture(GL_TEXTURE_2D, program->texture->texture);
     glBindVertexArray(program->vao);
@@ -41,9 +46,8 @@ int main(int argc, char *argv[])
 
     GLProgram prog   = GLInitProgram("../assets/images/debug16.png");
     GLShader shader  = GLShaderInit();
-    Camera3D camera  = Camera3DCreate((vec3){0.0f,0.0f,5.0f});
+    Camera3D camera  = Camera3DCreate((vec3){0.0f,0.0f,10.0f});
     GLProgram prog2   = GLInitProgram("../assets/images/debug32.png");
-
 
     prog.shader      = shader;
     prog2.shader     = shader;
@@ -52,14 +56,35 @@ int main(int argc, char *argv[])
     Entity e1 = {.position = {0,0,-10}, .color = {1,0,0}};
     Entity e2 = {.position = {4,2,-10}, .color = {1,1,0}};
 
-    const int MAX = 3;
+    const int MAX = 10;
     Entity entities[MAX][MAX];
 
     for(int x = 0; x<= MAX - 1; x++)
     {
         for(int y = 0; y<= MAX - 1; y++)
         {
-            Entity e = {.position = {2 * x,2 * y,-2}, .color = {1,x / 2.0,y / 2.0}};
+            int px = RandRangeI(-30,30);
+            int py = RandRangeI(-30,30);
+            int pz = RandRangeI(-30,30);
+
+            int cx = RandRangeI(0,1);
+            int cy = RandRangeI(0,1);
+            int cz = RandRangeI(0,1);
+
+            int rx = RandRangeI(0,360);
+            int ry = RandRangeI(0,360);
+            int rz = RandRangeI(0,360);
+
+            int sx = RandRangeI(1,4);
+            int sy = RandRangeI(1,4);
+            int sz = RandRangeI(1,4);
+
+            Entity e = {
+                .position = {px,py,pz}, 
+                .rotation = {rx,ry,rz},
+                .scale = {sx,sy,sz},
+                .color = {cx,cy,cz}
+            };
             entities[x][y] = e;
         }
     }
@@ -90,12 +115,12 @@ int main(int argc, char *argv[])
 
         glClear(GL_COLOR_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-        camera.position[0] = sin(c1) * 10.0;
-        camera.position[1] = cos(c1) * 10.0;
+        camera.position[0] = -sin(c1) * 10.0;
+        camera.position[1] = -cos(c1) * 10.0;
         camera.position[2] = cos(c1) * 10.0;
 
-        e2.position[0]     = sin(c1) * 3.0;
-        e2.position[1]     = cos(c1) * 3.0;
+        /*e2.position[0]     = sin(c1) * 3.0;*/
+        /*e2.position[1]     = cos(c1) * 3.0;*/
         /*e2.position[2]     = -cos(c1) * 10.0;*/
 
         for(int x = 0; x<= MAX - 1; x++)
